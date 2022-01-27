GhostlyDark/BizHawk

commit 319455c2ea791c90af16a99806dea816d980d455

Revert "TAStudio: Update self after loading a state. There doesn't seem to be any reason to not update TAStudio, and doing so fixes two bugs that were most evident when loading the state for frame 0:"
This reverts commit bb226f694aa9a3e2322c03dbdeb07f93e0774204.

This regressed #2965. Will attempt a different fix for the mentioned issues that doesn't regress anything.

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.IToolForm.cs
@@ -19,6 +19,7 @@ namespace BizHawk.Client.EmuHawk
 		[OptionalService]
 		public ISaveRam SaveRamEmulator { get; private set; }
 
+		private bool _hackyDontUpdate;
 		private bool _initializing; // If true, will bypass restart logic, this is necessary since loading projects causes a movie to load which causes a rom to reload causing dialogs to restart
 
 		private int _lastRefresh;
@@ -62,6 +63,11 @@ namespace BizHawk.Client.EmuHawk
 		protected override void UpdateAfter()
 		{
 			if (!IsHandleCreated || IsDisposed || CurrentTasMovie == null)
+			{
+				return;
+			}
+
+			if (_hackyDontUpdate)
 			{
 				return;
 			}

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.cs
@@ -961,16 +961,18 @@ namespace BizHawk.Client.EmuHawk
 				Emulator.ResetCounters();
 			}
 
-			UpdateTools();
+			UpdateOtherTools();
 		}
 
 		public void AddBranchExternal() => BookMarkControl.AddBranchExternal();
 		public void RemoveBranchExternal() => BookMarkControl.RemoveBranchExternal();
 
-		private void UpdateTools()
+		private void UpdateOtherTools() // a hack probably, surely there is a better way to do this
 		{
+			_hackyDontUpdate = true;
 			Tools.UpdateToolsBefore();
 			Tools.UpdateToolsAfter();
+			_hackyDontUpdate = false;
 		}
 
 		public void TogglePause()

commit efcd9275607bbe44a4c9a8794c6d7e10c93a3cab

TAStudio: Update self after loading a state. This fixes two bugs that were most evident when loading the state for frame 0: -The UI was not updated. -TasSession.UpdateValues was not called.  So saving the movie and then re-loading it would result in the previous frame being loaded/seeked to.
This commit addresses the regression mentioned in commit 319455c.

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.IToolForm.cs
@@ -19,7 +19,6 @@ namespace BizHawk.Client.EmuHawk
 		[OptionalService]
 		public ISaveRam SaveRamEmulator { get; private set; }
 
-		private bool _hackyDontUpdate;
 		private bool _initializing; // If true, will bypass restart logic, this is necessary since loading projects causes a movie to load which causes a rom to reload causing dialogs to restart
 
 		private int _lastRefresh;
@@ -63,11 +62,6 @@ namespace BizHawk.Client.EmuHawk
 		protected override void UpdateAfter()
 		{
 			if (!IsHandleCreated || IsDisposed || CurrentTasMovie == null)
-			{
-				return;
-			}
-
-			if (_hackyDontUpdate)
 			{
 				return;
 			}

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.ListView.cs
@@ -657,7 +657,6 @@ namespace BizHawk.Client.EmuHawk
 							CurrentTasMovie.SetBoolStates(firstSel, (frame - firstSel) + 1, buttonName, !allPressed);
 							_boolPaintState = CurrentTasMovie.BoolIsPressed(frame, buttonName);
 							_triggerAutoRestore = true;
-							JumpToGreenzone(true);
 							RefreshDialog();
 						}
 						else if (ModifierKeys == Keys.Shift && ModifierKeys == Keys.Alt) // Does not work?
@@ -671,7 +670,6 @@ namespace BizHawk.Client.EmuHawk
 							CurrentTasMovie.ToggleBoolState(TasView.CurrentCell.RowIndex.Value, buttonName);
 							_boolPaintState = CurrentTasMovie.BoolIsPressed(frame, buttonName);
 							_triggerAutoRestore = true;
-							JumpToGreenzone(true);
 							RefreshDialog();
 						}
 					}
@@ -846,10 +844,7 @@ namespace BizHawk.Client.EmuHawk
 					{
 						// If painting up, we have altered frames without loading states (for smoothness)
 						// So now we have to ensure that all the edited frames are invalidated
-						if (_paintingMinFrame < Emulator.Frame)
-						{
-							GoToFrame(_paintingMinFrame);
-						}
+						GoToLastEmulatedFrameIfNecessary(_paintingMinFrame); 
 					}
 
 					ClearLeftMouseStates();

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.cs
@@ -969,10 +969,8 @@ namespace BizHawk.Client.EmuHawk
 
 		private void UpdateOtherTools() // a hack probably, surely there is a better way to do this
 		{
-			_hackyDontUpdate = true;
 			Tools.UpdateToolsBefore();
 			Tools.UpdateToolsAfter();
-			_hackyDontUpdate = false;
 		}
 
 		public void TogglePause()