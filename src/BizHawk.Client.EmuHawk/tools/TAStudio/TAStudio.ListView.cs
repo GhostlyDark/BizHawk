GhostlyDark/BizHawk

commit 39c159fb6da1529b9353cdb2a6927f8f7465a0f7

Fix #2965, but for axis painting.

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.ListView.cs
@@ -840,7 +840,7 @@ namespace BizHawk.Client.EmuHawk
 				}
 				else
 				{
-					if (!string.IsNullOrWhiteSpace(_startBoolDrawColumn))
+					if (!string.IsNullOrWhiteSpace(_startBoolDrawColumn) || !string.IsNullOrWhiteSpace(_startAxisDrawColumn))
 					{
 						// If painting up, we have altered frames without loading states (for smoothness)
 						// So now we have to ensure that all the edited frames are invalidated
@@ -1164,10 +1164,7 @@ namespace BizHawk.Client.EmuHawk
 						}
 					}
 
-					var getVal = (i < CurrentTasMovie.InputLogLength) ? CurrentTasMovie.GetAxisState(i, _startAxisDrawColumn) : setVal;
 					CurrentTasMovie.SetAxisState(i, _startAxisDrawColumn, setVal); // Notice it uses new row, old column, you can only paint across a single column
-
-					if (getVal != setVal) { JumpToGreenzone(); }
 				}				
 			}
 

commit 48ab29913ec5c8db07ec5b340889288ae4925117

TAStudio: Do not seek to frame after clicking on an analog value. (regression from commit 39c159f)

src/BizHawk.Client.EmuHawk/tools/TAStudio/TAStudio.ListView.cs
@@ -16,6 +16,7 @@ namespace BizHawk.Client.EmuHawk
 		// Input Painting
 		private string _startBoolDrawColumn = "";
 		private string _startAxisDrawColumn = "";
+		private bool _drewAxis;
 		private bool _boolPaintState;
 		private int _axisPaintState;
 		private int _axisBackupState;
@@ -789,6 +790,7 @@ namespace BizHawk.Client.EmuHawk
 			_startSelectionDrag = false;
 			_startBoolDrawColumn = "";
 			_startAxisDrawColumn = "";
+			_drewAxis = false;
 			_paintingMinFrame = -1;
 			TasView.ReleaseCurrentCell();
 
@@ -840,7 +842,7 @@ namespace BizHawk.Client.EmuHawk
 				}
 				else
 				{
-					if (!string.IsNullOrWhiteSpace(_startBoolDrawColumn) || !string.IsNullOrWhiteSpace(_startAxisDrawColumn))
+					if (!string.IsNullOrWhiteSpace(_startBoolDrawColumn) || _drewAxis)
 					{
 						// If painting up, we have altered frames without loading states (for smoothness)
 						// So now we have to ensure that all the edited frames are invalidated
@@ -1165,7 +1167,9 @@ namespace BizHawk.Client.EmuHawk
 					}
 
 					CurrentTasMovie.SetAxisState(i, _startAxisDrawColumn, setVal); // Notice it uses new row, old column, you can only paint across a single column
-				}				
+				}
+
+				_drewAxis = true;
 			}
 
 			CurrentTasMovie.IsCountingRerecords = wasCountingRerecords;